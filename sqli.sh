a8ba58a47884c94ca457210da7d7102ed3b61ff7
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-09-10 04:06:43 +0530

    Update sqli.sh

--- sqli.sh
+++ sqli.sh
@@ -1,3 +1,4 @@
-gau $1 | httpx -silent -timeout 2 -threads 500 | grep "=" | grep -v ".jpg\|.jpeg\|.gif\|.css\|.tif\|.tiff\|.png|.ttf\|.woff\|.woff2\|.ico\|.pdf\|.woff2\|.svg\|.js" | qsreplace | anew | tee -a $1urls.txt;
-cat $1urls.txt | gf sqli | tee -a $1sqliurls.txt;
-jaeles scan -c 1000 -v -s ../../jaeles-signatures/fuzz/sqli/sql-error.yaml -U $1sqliurls.txt -o $1SqliResults/
+gau $1 | httpx -silent -timeout 2 -threads 500 | grep "=" | grep -v ".jpg\|.jpeg\|.gif\|.css\|.tif\|.tiff\|.png|.ttf\|.woff\|.woff2\|.ico\|.pdf\|.woff2\|.svg\|.js" | qsreplace | anew | tee -a gauurls.txt;
+gospider -s https://$1 -d 10 -t 20 -a -w --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|woff2|svg|js)" | grep -oP "(http[s]?:\/\/)?((-)?[\w+\.]){1,}$1.*" | grep "=" | qsreplace payload | anew | sort -u | urlprobe -c 500 -t 01 | grep 200 | awk '{print $5}' | tee -a gospiderurls.txt
+cat gauurls.txt gospiderurls.txt | sort -u | allurls.txt;
+jaeles scan -c 1000 -v -s ../../jaeles-signatures/fuzz/sqli/sql-error.yaml -U allurls.txt -o SqliResults/
