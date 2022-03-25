c7aee55d71a64060301c85bbdd301ddd6825d1df
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2022-03-26 02:45:15 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,10 +1,10 @@
-gospider -S domain -d 16 -a -c 250 | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?gr8people.com.*" | gf xss | uro > xssurls.txt
-wingman -l xssurls.txt --progress
-rm xssurls.txt
+gospider -S domain -d 16 -a -c 250 | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?gr8people.com.*" | gf xss | uro > gourls.txt
 cat domain | waybackurls | gf xss | uro > waybackxssurls.txt;
-wingman -l waybackxssurls.txt --progress
-rm waybackxssurls.txt
 cat domain | gau | gf xss | uro > gauurls.txt;
-wingman -l gauurls.txt --progress
-rm gauurls.txt
+cat gourls.txt waybackxssurls.txt gauurls.txt | uro > xssurls.txt
+wingman -l xssurls.txt --progress
 wingman -l domain --crawl --progress
+rm xssurls.txt
+rm waybackxssurls.txt
+rm gauurls.txt
+rm gourls.txt
