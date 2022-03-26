147062d563688ee18759d77e45492fb24d9e47ec
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2022-03-26 06:21:53 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,10 +1,12 @@
-gospider -S domain -d 16 -a -c 250 | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?gr8people.com.*" | gf xss | uro > gourls.txt
-cat domain | waybackurls | gf xss | uro > waybackxssurls.txt;
-cat domain | gau | gf xss | uro > gauurls.txt;
-cat gourls.txt waybackxssurls.txt gauurls.txt | uro > xssurls.txt
-wingman -l xssurls.txt --progress
-wingman -l domain --crawl --progress
+for subdomain in $(cat domain);do
+gospider -s $subdomain -d 16 -a -c 250 | grep "$subdomain" | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?.*" | gf xss | uro | anew gourls.txt
+echo $subdomain | waybackurls | gf xss | uro | anew waybackxssurls.txt;
+echo $subdomain | gau | gf xss | uro | anew gauurls.txt;
+cat gourls.txt waybackxssurls.txt gauurls.txt | uro | anew xssurls.txt
+wingman -l xssurls.txt --progress -json | notify
+wingman -u $subdomain --crawl --progress --c-threads 50 -json | anew notify
 rm xssurls.txt
 rm waybackxssurls.txt
 rm gauurls.txt
 rm gourls.txt
+done
