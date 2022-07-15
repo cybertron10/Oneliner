ec6dee0d700648f8adf06ec19f53613d9dae4444
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2022-07-15 17:50:09 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -18,8 +18,7 @@ gospider -s $subdomain -d 16 -a -c 250 | grep "$subdomain" | grep -oP "http(s)?:
 echo $subdomain | waybackurls | gf xss | uro | anew waybackxssurls.txt;
 echo $subdomain | gau | gf xss | uro | anew gauurls.txt;
 cat gourls.txt waybackxssurls.txt gauurls.txt | uro | anew xssurls.txt
-wingman -l xssurls.txt --progress -json | notify
-wingman -u $subdomain --crawl --progress --c-threads 50 -json | anew notify
+jaeles scan -c 1000 -v -s /root/mysignature/xss.yaml -U xssurls.txt -o output
 rm xssurls.txt
 rm waybackxssurls.txt
 rm gauurls.txt
