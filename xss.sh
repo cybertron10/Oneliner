d8ddfd63c4ef055e76af696c054f1959c39c60b3
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2022-07-15 17:50:59 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -14,7 +14,7 @@ jaeles scan -c 1000 -v -s /root/mysignature/x-forwarded-host.yaml -U domain -o c
 cat cachepoison | toslack
 echo "Jaeles Scan Completed" | toslack
 for subdomain in $(cat domain);do
-gospider -s $subdomain -d 16 -a -c 250 | grep "$subdomain" | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?.*" | gf xss | uro | anew gourls.txt
+gospider -s $subdomain -t 15 -d 16 -a -c 250 | grep "$subdomain" | grep -oP "http(s)?://((?i)(([a-zA-Z0-9]{1}|[_a-zA-Z0-9]{1}[_a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1})[.]{1})+)?.*" | gf xss | uro | anew gourls.txt
 echo $subdomain | waybackurls | gf xss | uro | anew waybackxssurls.txt;
 echo $subdomain | gau | gf xss | uro | anew gauurls.txt;
 cat gourls.txt waybackxssurls.txt gauurls.txt | uro | anew xssurls.txt
